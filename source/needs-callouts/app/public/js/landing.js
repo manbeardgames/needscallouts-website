6ecae2b3640c1a8c79350ab9ff48e45df814dd72
Christopher Whitley <manbeardgames@gmail.com>
2022-02-11 23:27:35 -0500

    fuck making this gallery for some reason

--- source/needs-callouts/app/public/js/landing.js
+++ source/needs-callouts/app/public/js/landing.js
@@ -1,3 +1,12 @@
+//  Cachable references to the last known scorll positions
+let lastScrollY = 0;
+
+//  Cached refrence to all sections
+let sections = document.getElementsByClassName('section');
+
+//  Cached reference to the current section id
+let currentSection = 0;
+
 const debounce = (fn) => {
     //  Holds the requestAnimationFrame reference in case we want to cancel it
     let frame;
@@ -17,19 +26,33 @@ const debounce = (fn) => {
 
 //  Reads scroll position and stores it in a data attribute
 const onScroll = () => {
-    let nav = document.getElementsByClassName('full-screen-navigation')[0];
-    let mobileNav = document.getElementsByClassName('mobile-navigation')[0];
-    let illustration = document.getElementsByClassName('illustration')[0];
-    let targetScroll = illustration.offsetTop + illustration.offsetHeight - 128;
-    if(window.scrollY >= targetScroll) {
-        // nav.dataset.background = 'dark';
-        mobileNav.dataset.background = nav.dataset.background = 'dark';
-    } else {
-        mobileNav.dataset.background = nav.dataset.background = 'transparent';
-        // nav.dataset.background = 'transparent';
-    }
-}
+    // //  Get the current scroll value
+    // let currentScroll = window.scrollY;
 
+    // //  Determine scroll direction
+    // let sectionOffset = currentScroll >= lastScrollY ? 1 : currentScroll <= lastScrollY ? -1 : 0;
+
+    // //  Determine the next section to scroll to
+    // currentSection = Math.min(Math.max(currentSection + sectionOffset, 0), sections.length - 1);
+
+    // lastScrollY = currentScroll;
+
+    // //  Scroll to that section
+    // window.scrollTo(0, sections[currentSection].offsetTop);
+
+
+    // let nav = document.getElementsByClassName('full-screen-navigation')[0];
+    // let mobileNav = document.getElementsByClassName('mobile-navigation')[0];
+    // let illustration = document.getElementsByClassName('illustration')[0];
+    // let targetScroll = illustration.offsetTop + illustration.offsetHeight - 128;
+    // if (window.scrollY >= targetScroll) {
+    //     // nav.dataset.background = 'dark';
+    //     mobileNav.dataset.background = nav.dataset.background = 'dark';
+    // } else {
+    //     mobileNav.dataset.background = nav.dataset.background = 'transparent';
+    //     // nav.dataset.background = 'transparent';
+    // }
+};
 
 //  Listen for new scroll events
 document.addEventListener('scroll', debounce(onScroll), { passive: true });
